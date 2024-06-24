b232bd7f8054bb6bd9df9c4052c14720effb2f99
DasAvocado <an.knittel@duocuc.cl>
2024-06-24 12:39:17 -0400

    Add files via upload
    se añadieron todas las opciones y añadiendo el archivo csv automaticamente

--- funciones.py
+++ funciones.py
@@ -1,21 +1,31 @@
+import csv
+import creacionArchivo as apoyo
+
 lista_productos=[]
-def login():
-    lista_usuarios={        
-        "antonk":"1234",
-        "thomasm":"5678",
-        "caldo":"1133"}
-def agregar_productos():    
+def agregar_productos():
+    codigo=input("Ingrese el codigo de producto")
     producto=input("Ingrese el nuevo producto")
     lista_productos.append(producto)
-    print(lista_productos)
+    precio=input("Ingrese el precio del producto")
+    cantidad=input("Ingrese la cantidad de productos")
+    apoyo.agregar_producto
+    print("Su producto fue añadido exitosamente")
 
 def mostrar_productos():
     print("")
 
 def mod_producto():
     producto=input("Ingrese el producto a modificar")
-    producto in lista_productos
-    lista_productos.remove(producto)
+    producto_nuevo=input("Ingrese el nuevo producto\n")
+    modificar=lista_productos.index(producto)
+    lista_productos[modificar]=producto_nuevo
+    print(f"El producto {producto} a sido modificado e intercambiado por {producto_nuevo}")
 
 def borrar_producto():
     producto=input("Ingrese el producto a eliminar")
+    lista_productos.remove(producto)
+
+
+def salir():
+    print("Usted ingreso a la opcion de salir del programa.")
+    print("Saliendo del programa.")
