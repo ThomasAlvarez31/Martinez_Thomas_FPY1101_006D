1ffdd4dbd770ff0d5c2d9a743af65f4a4f9f6faf
Thomas31 <163472229+ThomasAlvarez31@users.noreply.github.com>
2024-06-24 12:12:32 -0400

    actualizacion
    nuevas variables

--- creacionArchivo.py
+++ creacionArchivo.py
@@ -3,5 +3,12 @@ import csv
 def agregar_producto(codigo, nombre, precio, cantidad):
     with open ('productos.csv','a',newline='')as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow([codigo,nombre, precio, cantidad])
-    agregar_producto(nombre,precio,cantidad)
+        writer.writerow([codigo,producto, precio, cantidad])
+datos.insert(0, ['codigo','producto','precio','cantidad'])
+
+while True:
+    producto = input("Ingrese el nombre")
+    precio = float(input("Ingrese el precio"))
+    cantidad = int(input("Ingrese la cantidad del producto"))
+    codigo = int(input("Ingrese el codigo"))
+    agregar_producto(codigo,producto,precio,cantidad)
