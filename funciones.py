4f21602306eb68ef15f064063467f821d72aee98
DasAvocado <an.knittel@duocuc.cl>
2024-06-24 12:00:25 -0400

    Add files via upload
    me esta dando caleta de errores para subirlo ayuda

--- funciones.py
+++ funciones.py
@@ -1,3 +1,25 @@
+<<<<<<< HEAD
+lista_productos=[]
+def agregar_productos():    
+    producto=input("Ingrese el nuevo producto")
+    lista_productos.append(producto)
+    print(lista_productos)
+
+def mostrar_productos():
+    print("")
+
+def mod_producto():
+    producto=input("Ingrese el producto a modificar")
+    producto_nuevo=input("Ingrese el nuevo producto\n")
+    modificar=lista_productos.index(producto)
+    lista_productos[modificar]=producto_nuevo
+    print(f"El producto {producto} a sido modificado e intercambiado por {producto_nuevo}")
+
+def borrar_producto():
+    producto=input("Ingrese el producto a eliminar")
+    lista_productos.remove(producto)
+
+=======
 lista_productos=[]
 def login():
     lista_usuarios={        
@@ -19,3 +41,4 @@ def mod_producto():
 
 def borrar_producto():
     producto=input("Ingrese el producto a eliminar")
+>>>>>>> 4b2dcc800a23cf95d47a62d9997dfc0e634f1dfa
