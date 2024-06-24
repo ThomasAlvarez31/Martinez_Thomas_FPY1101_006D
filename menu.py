1aed30ce4483f5d2e32027a3e9fc8139f44f8a27
Catal2s <164356150+Catal2s@users.noreply.github.com>
2024-06-24 12:39:55 -0400

    menu.py

--- menu.py
+++ menu.py
@@ -13,17 +13,17 @@ while True:
         print("\nDebe ingresar un numero del 1 al 5.")
 #Opcion 1
     if opcion==1:
-        funcion.agregar_productos
+        funcion.agregar_productos()
 #Opcion 2
     elif opcion==2:
-        funcion.mostrar_productos
+        funcion.mostrar_productos()
 #Opcion 3
     elif opcion==3:
-        funcion.mod_producto
+        funcion.mod_producto()
 #Opcion 4
     elif opcion==4:
         print("Usted ingreso a la opcion de eliminar producto.")
-        funcion.borrar_producto
+        funcion.borrar_producto()
 #Opcion 5
     elif opcion==5:
         funcion.salir
