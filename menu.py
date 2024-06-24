9127b5e145f6700505e9ad150b7b7f8eb4781ce8
Catal2s <ja.cataldo@duocuc.cl>
2024-06-24 11:25:19 -0400

    menuu

--- menu.py
+++ menu.py
@@ -1,10 +1,28 @@
+
 #menu
 while True:
     print("\nInventario Tienda");
-    print("\n1.-Agregar un producto.\n2.-Ver todos los productos.\n3.-Modificar un producto.\n4.-Eliminar un producto.\n5.-Guardar coleecion en un archivo.\n6.-Salir del programa.")
+    print("\n1.-Agregar un producto.\n2.-Ver todos los productos.\n3.-Modificar un producto.\n4.-Eliminar un producto.\n5.-Guardar coleccion en un archivo.\n6.-Salir del programa.")
     try:
         opcion=int(input("\nIngrese una opcion: "))
     except ValueError:
         print("\nDebe ingresar un numero del 1 al 6.")
     if opcion==1:
-        print("\n")
+        print("\n¿Que producto desea agregar al iventario de la lista?")
+        agregar_producto=input("Ingrese el producto:")
+    elif opcion==2:
+        print("")
+    elif opcion==3:
+        print("Usted ingreso a la opcion de modificar un producto.")
+        input("Ingrese el producto que desea modificar")
+    elif opcion==4:
+        print("Usted ingreso a la opcion de eliminar producto.")
+        input("¿Que producto desea eliminar?\n")
+    elif opcion==5:
+        print("Usted ingreso a la opcion de guardar coleccion en un archivo.")
+    elif opcion==6:
+        print("Usted ingreso a la opcion de salir del programa")
+        print("Saliendo del programa.")
+        break
+    else:
+        print("Debe ingresar un numero del 1 al 6.")
